c82c098a38b3ece4cf3aa1525b71e0aa3153053c
Rishit-Thaper <rishitthaper5329@gmail.com>
2023-08-17 18:23:10 +0530

    first

---
+++ JS/main.js
@@ -0,0 +1,104 @@
+//function to add Background Color to the Navbar
+
+document.addEventListener("DOMContentLoaded", function () {
+    const navbar = document.querySelector("header");
+    const header = document.querySelector(".home");
+    const headerHeight = header.offsetHeight;
+    let isNavbarTransparent = true;
+  
+    function updateNavbarBackground() {
+      const scrollY = window.scrollY;
+      const shouldNavbarBeTransparent = scrollY < headerHeight;
+  
+      if (shouldNavbarBeTransparent !== isNavbarTransparent) {
+        isNavbarTransparent = shouldNavbarBeTransparent;
+        requestAnimationFrame(() => {
+          navbar.style.transition = "background-color 0.3s ease"; 
+          navbar.style.backgroundColor = isNavbarTransparent
+            ? "transparent"
+            : "#44261e"; 
+        });
+      }
+    }
+
+function debounce(func, wait) {
+  let timeout;
+  return function () {
+    const context = this;
+    const args = arguments;
+    clearTimeout(timeout);
+    timeout = setTimeout(() => func.apply(context, args), wait);
+  };
+}
+
+const debouncedUpdateNavbarBackground = debounce(updateNavbarBackground, 10);
+    window.addEventListener("scroll", debouncedUpdateNavbarBackground);
+});
+  
+  
+//Function to make Navbar Responsive
+function toggleMenu() {
+
+  console.log("clicked")
+  var navMenu = document.querySelector(".nav-menu");
+
+  if (navMenu.classList.contains("active")) {
+    navMenu.classList.remove("active");
+    navMenu.style.transform = "translateY(-200px)";
+  } else {
+    navMenu.classList.add("active");
+    navMenu.style.transform = "translateY(0)";
+  }
+}
+
+//Function to make Image Slideshow
+
+const imageContainer = document.querySelector('.image-container');
+const imageUrls = ["Rest/slide-banner-3.jpg", "Rest/slide-banner-2.jpg", "Rest/slide-banner-1.jpg"];
+let currentImageIndex = 0;
+
+function updateImage() {
+  imageContainer.style.backgroundImage = `url(${imageUrls[currentImageIndex]})`;
+
+  currentImageIndex = (currentImageIndex + 1) % imageUrls.length;
+}
+
+setInterval(updateImage, 3000); 
+updateImage();
+
+
+
+//Function to make Animation on Scroll
+function animateOnScroll(elements) {
+    const observer = new IntersectionObserver((entries, observer) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          entry.target.classList.add('animate__animated', 'animate__fadeInUp');
+          observer.unobserve(entry.target);
+        }
+      });
+    });
+  
+    elements.forEach((item) => {
+      observer.observe(item);
+    });
+}
+
+const aboutSection = document.querySelectorAll('.about-section');
+const featureSection = document.querySelectorAll('.main-grid');
+const menuSection = document.querySelectorAll('.menu-grid');
+const interiorSection = document.querySelectorAll('.interior-grid');
+const dishesSection = document.querySelectorAll('.dishes-grid');
+
+animateOnScroll(aboutSection);
+animateOnScroll(featureSection);
+animateOnScroll(interiorSection);
+animateOnScroll(menuSection);
+animateOnScroll(dishesSection);
+
+
+
+
+
+
+
